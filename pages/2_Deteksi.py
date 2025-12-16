7c74f5f9ac21200c1a559dc780118ec734008b74
Tabina Shafa <tabinashafa@gmail.com>
2025-12-16 08:47:53 +0700

    update

--- pages/2_Deteksi.py
+++ pages/2_Deteksi.py
@@ -12,11 +12,11 @@ import base64
 # Load the trained model
 # Ubah ini dengan H5 mu Cap! 
 model_katarak = tf.keras.models.load_model('model/model_mobilenet_katarak_new.keras', compile=False)
-model_fundus = tf.keras.models.load_model('model/model_mobilenet_katarak_new.keras', compile=False)
-# model_fundus = tf.keras.models.load_model('model/model_mobilenet_fundus.keras', compile=False)
+model_fundus = tf.keras.models.load_model('model/model_mobilenet_fundus.keras', compile=False)
 
 # Resize atur juga jgn lupa, samakan dengan model mu make pixel berapa!!
 # Fungsi preprocessing untuk CLAHE dan konversi ke grayscale
+st.text(model_katarak.summary())
 
 def preprocessing(img):
     image = np.array(img.resize((180, 180)))
